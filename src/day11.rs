265ec8e308196b271e00c170d28881e742357d5c
Christopher MacGown <chris@glassbear.com>
2018-12-11 22:58:25 -0800

    Fixed Day 11.
    The summed-area table was being calculated properly, my problem was that
    I wasn't using the data properly. I was trying to add the values
    directly instead of using the formula to extract the area from the
    encoded values.

--- src/day11.rs
+++ src/day11.rs
@@ -28,8 +28,6 @@ impl FuelGrid {
             for x in 0..300 {
                 let idx = index_at(x, y);
 
-                println!("{:?}", table.get(index_at(x - 1, y)).unwrap_or(&0));
-
                 table[idx] = table.get(index_at(x - 1, y)).unwrap_or(&0)
                     + table.get(index_at(x, y - 1)).unwrap_or(&0)
                     - table.get(index_at(x - 1, y - 1)).unwrap_or(&0)
@@ -42,12 +40,44 @@ impl FuelGrid {
 
     fn get(&self, x: isize, y: isize) -> isize {
         let idx = index_at(x, y);
-        if idx < 0 || idx > 90_000 {
-            // OUT OF BOUNDS
-            0
-        } else {
-            *self.table.get(idx).unwrap()
+
+        *self.table.get(idx).unwrap_or(&0)
+    }
+
+    fn max_subsquare(&self, only_three: bool) -> (isize, (isize, isize, isize)) {
+        let mut max_score = 0;
+        let mut max_size = 0;
+        let mut max_coords = (0, 0);
+
+        let sizes = if only_three { 3..4 } else { 1..300 };
+        for y in 0..300 {
+            for x in 0..300 {
+                for size in sizes.clone() {
+                    let (x_n, y_n) = (x + size - 1, y + size - 1);
+                    if x_n >= 300 || y_n >= 300 {
+                        continue;
+                    }
+
+                    let current = self.total((x, y), (x_n, y_n));
+                    if current > max_score {
+                        max_score = current;
+                        max_size = size;
+                        max_coords = (x, y);
+                    }
+                }
+            }
         }
+
+        (max_score, (max_coords.0, max_coords.1, max_size))
+    }
+
+    fn total(&self, (x1, y1): (isize, isize), (x2, y2): (isize, isize)) -> isize {
+        let a = self.get(x1 - 1, y1 - 1);
+        let b = self.get(x1 - 1, y2);
+        let c = self.get(x2, y1 - 1);
+        let d = self.get(x2, y2);
+
+        d - b - c + a
     }
 }
 
@@ -59,170 +89,8 @@ pub fn run(input: Lines) -> Result<(), Error> {
         .parse::<isize>()?;
 
     let grid = FuelGrid::new(serial);
-
-    //    println!("{:?}", grid);
-
-    Ok(())
-}
-
-/*
-const WIDTH: isize = 300;
-
-type SubGridCost = Vec<(isize, isize, (isize, isize))>;
-
-enum Square {
-    Fixed,  // 3x3
-    Custom, // NxN where 1 ≤ N ≤ 300
-}
-
-fn index(row: isize, col: isize) -> usize {
-    ((row - 1) * WIDTH + (col - 1)) as usize
-}
-
-fn get_neighbors(row: isize, col: isize, size: isize) -> Vec<(isize, isize)> {
-    let mut neighbors = Vec::new();
-    for r in 0..size {
-        for c in 0..size {
-            neighbors.push((col + c, row + r))
-        }
-    }
-
-    neighbors
-}
-
-#[derive(Debug)]
-struct FuelGrid(Vec<isize>);
-impl FuelGrid {
-    fn new(serial: isize) -> Self {
-        let mut grid = vec![0; 90_000];
-
-        for row in 1..301 {
-            for col in 1..301 {
-                let rack_id = col + 10;
-                let base_power = row * rack_id;
-                let scaled_power = (base_power + serial) * rack_id;
-
-                let power = ((scaled_power / 100) % 10) - 5;
-                println!("{:?}", index(row, col));
-                grid[index(row, col)] = power
-                    + *grid.get(index(row - 1, col)).unwrap_or(&0)
-                    + *grid.get(index(row, col - 1)).unwrap_or(&0)
-                    + *grid.get(index(row - 1, col - 1)).unwrap_or(&0);
-            }
-        }
-
-        FuelGrid(grid)
-    }
-
-    fn subgrids(&self, square: Square) -> SubGridCost {
-        let square_sizes = match square {
-            Square::Fixed => vec![3],
-            Square::Custom => (1..301).collect::<Vec<_>>(),
-        };
-
-        let mut subgrids = Vec::new();
-        for row in 1..301 {
-            for col in 1..301 {
-                let scored: Vec<(isize, isize, (isize, isize))> = square_sizes
-                    .iter()
-                    .map(|s| {
-                        let neighbors = get_neighbors(row, col, *s);
-                        let score = neighbors.iter().fold(0, |mut score, &(row, col)| {
-                            if let Some(power_level) = self.get(col, row) {
-                                score += power_level;
-                            }
-                            score
-                        });
-
-                        let (row, col) = neighbors[0];
-
-                        (score, *s, (col, row))
-                    })
-                    .collect();
-                subgrids.push(scored);
-            }
-        }
-
-        subgrids.into_iter().flat_map(|v| v).collect()
-    }
-
-    fn sorted_subgrids(&self, square: Square) -> SubGridCost {
-        let mut subgrids = self.subgrids(square);
-
-        subgrids.sort_by_key(|&(score, _, _)| score);
-        subgrids
-    }
-
-    fn get(&self, x: isize, y: isize) -> Option<&isize> {
-        self.0.get(index(y, x))
-    }
-}
-
-pub fn run(input: Lines) -> Result<(), Error> {
-    let mut input = input;
-    let serial = input
-        .next()
-        .ok_or(format_err!("expected an input"))?
-        .parse::<isize>()?;
-
-    let grid = FuelGrid::new(serial);
-    let subgrids = grid.sorted_subgrids(Square::Fixed);
-
-    println!("{:?}", subgrids.len());
-    println!("{:?}", subgrids[subgrids.len() - 1]);
+    println!("I: {:?}", grid.max_subsquare(true));
+    println!("II: {:?}", grid.max_subsquare(false));
 
     Ok(())
 }
-
-#[cfg(test)]
-mod tests {
-    use super::{index, index_to_coords, FuelGrid, Square};
-
-    #[test]
-    fn test_round_trip_index() {
-        assert_eq!((64, 243), index_to_coords(index(243, 64)));
-    }
-
-    #[test]
-    fn test_expected_power_levels() {
-        let tests = vec![
-            (57, (122, 79), -5),
-            (39, (217, 196), 0),
-            (71, (101, 153), 4),
-        ];
-
-        for (serial, (col, row), expected) in tests {
-            let grid = FuelGrid::new(serial);
-            assert_eq!(Some(&(expected as isize)), grid.get(col, row))
-        }
-    }
-
-    #[test]
-    fn test_grid() {
-        let tests = vec![(18, 29), (42, 30)];
-
-        for (serial, expected_power) in tests {
-            let grid = FuelGrid::new(serial);
-            let subgrids = grid.sorted_subgrids(Square::Fixed);
-
-            let (actual, _, _) = subgrids[90_000 - 1];
-
-            assert_eq!(expected_power, actual);
-        }
-    }
-
-    fn test_sized_grid() {
-        let tests = vec![(18, 113, (90, 269, 16)), (42, 119, (232, 251, 12))];
-
-        for (serial, expected_power, candidate) in tests {
-            let grid = FuelGrid::new(serial);
-            let subgrids = grid.sorted_subgrids(Square::Custom);
-
-            let (actual, _, _) = subgrids[90_000 - 1];
-
-            assert_eq!(expected_power, actual);
-        }
-    }
-
-}
-*/
